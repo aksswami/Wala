95f26be8448d442466f2d076ce6c5d109cbc3bfd
Amit kumar Swami <amit@moldedbits.com>
2015-01-10 14:48:04 +0530

    Initial Commit

---
+++ Wala/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Wala
+//
+//  Created by Amit kumar Swami on 10/01/15.
+//  Copyright (c) 2015 Amit kumar Swami. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ Wala/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Wala
+//
+//  Created by Amit kumar Swami on 10/01/15.
+//  Copyright (c) 2015 Amit kumar Swami. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
